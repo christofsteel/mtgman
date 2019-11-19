5b3a7af91f26c610a517cd8003ed90ce5916376e
Christoph Stahl <christoph.stahl@uni-dortmund.de>
2019-11-19 09:20:02 +0100

    Initial scryfall search

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="mtgman",
+        version="0.1",
+        packages=find_packages(),
+        author="Christoph Stahl",
+        author_email="christoph.stahl@tu-dortmund.de",
+        install_requires=["sqlalchemy", "scrython"],
+        entry_points = {
+            "console_scripts": [
+                "mtgman = mtgman.main:main"
+                ]
+            }
+        )
